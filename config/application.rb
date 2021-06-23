23ee8ba1fa52fe09ea859b1354082e4cbe86d570
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-06-23 14:49:36 +0900

    タイムゾーンの設定

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ Bundler.require(*Rails.groups)
 module Reminder
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.generators do |g|
       g.test_framework :rspec,
                    model_specs: true,
