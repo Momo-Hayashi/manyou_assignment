776bee41f6bbdff6c541f26e33d40ad2795e18cb
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-06-24 11:39:30 +0900

    SystemSpec修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,13 +1,8 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
 
-  # let!(:task) { FactoryBot.create(:task ) }
-  # let!(:task) { FactoryBot.create(:second_task ) }
-
-  before do
-    FactoryBot.create(:task)
-    FactoryBot.create(:second_task)
-  end
+  let!(:task) { FactoryBot.create(:task ) }
+  let!(:second_task) { FactoryBot.create(:second_task ) }
 
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -23,15 +18,14 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
 
   describe '一覧表示機能' do
+    before { visit tasks_path }
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        visit tasks_path
         expect(page).to have_content 'task1'
       end
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
-        visit tasks_path
         task_list = all('.task_row')
         expect(task_list[0]).to have_content 'task2'
         expect(task_list[1]).to have_content 'task1'
@@ -42,9 +36,8 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
-        task = FactoryBot.create(:task, name: 'task3', detail: 'test3')
-        visit task_path(task.id)
-        expect(page).to have_content 'test3'
+        visit task_path(second_task.id)
+        expect(page).to have_content 'test2'
       end
     end
   end
