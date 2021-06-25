caa636f492a43f52dad6cdea5d22f1c9f88d7f94
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-06-25 16:50:36 +0900

    Create : Created User Controller and Session Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'tasks#index'
   resources :tasks
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :destroy, :show]
 end
