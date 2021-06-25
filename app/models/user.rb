c736f2c032d3a8dc833370cc864fd40812c41e94
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-06-25 16:18:03 +0900

    Create : Created SeedsData to Users, and added Has_Secure_Password method to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
