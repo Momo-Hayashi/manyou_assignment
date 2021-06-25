e0b3cfcbb0a27794323e1128340604644311befc
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-06-25 16:07:11 +0900

    Create : Created UserModel and Validation for Users

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 2021_06_25_014300) do
+ActiveRecord::Schema.define(version: 2021_06_25_070434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -12,6 +12,15 @@ ActiveRecord::Schema.define(version: 2021_06_25_014300) do
     t.integer "status", default: 0, null: false
     t.integer "priority", default: 3, null: false
     t.index ["name"], name: "index_tasks_on_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
