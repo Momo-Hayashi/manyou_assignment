b793e1272a12a4317379c91da5d0b464e2413984
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-06-28 15:17:04 +0900

    Create : Created Admin functions to Views and Controller

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,47 @@
+class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  def index
+    @users = User.all.includes(:tasks)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice:'<< Successfully created new user >>'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice:'<< Successfully updated the user! >>'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path, notice: "<< Successfully deleted the user >>"
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :tasks
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :destroy, :show]
+  namespace :admin do
+    resources :users
+  end
 end
