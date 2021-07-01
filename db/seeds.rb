f5d75eb1f6efa5c99568bfe464df5d51da88ffa6
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-07-01 12:23:59 +0900

    Add: Made seends deta, 10 each of user, task, and label

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,28 @@
-# 50.times do |n|
-#   name = Faker::Games::Pokemon.name
-#   email = Faker::Internet.email
-#   password = "password"
-#   User.create!(name: name,
-#                email: email,
-#                password: password,
-#                )
+10.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               )
+end
 
-# User.create!(name:  "管理者",
-#              email: "admin@example.jp",
-#              password:  "11111111",
-#              password_confirmation: "11111111",
-#              admin: true)
+User.create!(name:  "管理者",
+             email: "admin@example.jp",
+             password:  "11111111",
+             password_confirmation: "11111111",
+             admin: true)
 
-5.times do |n|
-  Label.create!(label_name: '仕事')
+10.times do |n|
+  label_name = Faker::Food.fruits
+  Label.create!( label_name: label_name )
+end
+
+10.times do |n|
+  name = Faker::Movie.title
+  detail = Faker::Movie.quote
+  Task.create!(name: name,
+               detail: detail,
+               )
 end
